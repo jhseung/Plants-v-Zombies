fd2d2e3dfc0b2afa8da24e17a82c2f42bc919dad
Jaclyn Huang <mh2279@cornell.edu>
2018-03-29 15:30:19 -0400

    Update command.mli

--- command.mli
+++ command.mli
@@ -2,7 +2,7 @@ open State
 
 (*[cards] is the cards to move; [src] is the pile (source) where the cards are;
   [dst] is the pile (destination) where the cards are going.*)
-type move = {cards : pile; src : pile; dst : pile}
+type move = {cards : card list; src : card list; dst : card list}
 
 type command = Look | Move of move | Flip | Restart | Quit | Moves of int
              | Undo | Hint
