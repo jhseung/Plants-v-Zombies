d72e6b1d8f03cc4f9c3bea82be5acce54084fe46
Jaclyn Huang <mh2279@cornell.edu>
2018-04-16 16:32:00 -0400

    Update tile.mli

--- tile.mli
+++ tile.mli
@@ -6,7 +6,7 @@ type object = |Zombie of zombie |Bean of bean
 val init_tile: int -> int -> int -> tile
 
 (* Checks if an object is on a tile *)
-val in_range: tile -> object -> bool
+val on_tile: tile -> object -> bool
 
 (* Moves the zombie from the current tile to the next tile *)
 val move_zombie: zombie -> unit
