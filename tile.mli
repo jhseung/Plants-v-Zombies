61cf0b6d9f08f873aa1d98f09798eb74610f2dbf
Jaclyn Huang <mh2279@cornell.edu>
2018-04-16 15:56:59 -0400

    Update tile.mli

--- tile.mli
+++ tile.mli
@@ -1,1 +1,11 @@
-type tile = {zombies: zombie list; plant: plant option}
+type tile = {x: int; y: int; size: int; mutable zombies: zombie list; mutable plant: plant option}
+
+type object = |Zombie of zombie |Bean of bean
+
+val init_tile: int -> int -> int -> tile
+
+(* Checks if an object is on a tile *)
+val in_range: tile -> object -> bool
+
+(* Moves the zombie from the current tile to the next tile *)
+val move_zombie: zombie -> unit
