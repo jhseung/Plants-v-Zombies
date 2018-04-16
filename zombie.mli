b6b51230be47bfd9d7036deeea788eeb4bfd925d
Jaclyn Huang <mh2279@cornell.edu>
2018-04-16 19:37:29 -0400

    Update zombie.mli

--- zombie.mli
+++ zombie.mli
@@ -1,4 +1,4 @@
-type zombie = {mutable pos: tile; mutable HP: int}
+type zombie = {mutable pos: tile; mutable hp: int}
 
 (* Moves the zombie from the current tile to the next tile *)
 val move_zombie: zombie -> unit
