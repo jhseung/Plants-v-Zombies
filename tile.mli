b0e42c9f11372023633da860b4cade0d9feae6ef
Jaclyn Huang <mh2279@cornell.edu>
2018-04-17 11:16:35 -0400

    Update tile.mli

--- tile.mli
+++ tile.mli
@@ -1,7 +1,7 @@
 type tile = {x: int; y: int; size: int; mutable zombies: zombie list; 
 mutable plant: plant option; mutable left: tile; mutable right: tile}
 
-type object = |Zombie of zombie |Bean of bean
+type object = |Zombie of zombie |Projectile of projectile
 
 val init_tile: int -> int -> int -> tile
 
