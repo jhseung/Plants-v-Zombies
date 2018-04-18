18dc71ce99b0580a5589f37a7bd358d8c52d278f
Jaclyn Huang <mh2279@cornell.edu>
2018-04-17 23:03:34 -0400

    Update state.mli

--- state.mli
+++ state.mli
@@ -1,6 +1,4 @@
-type tile
-
-type state = (tile array) list
+type state = (tile array) array
 
 (* [rep_ok s] determines whether the current state [s] of the program is
  * valid
@@ -17,4 +15,7 @@ val score : state -> int
 (* [do' c s] returns a new state [s'] that represents the new state after a command [c]
  * has been made on state [s]. Returns [s] if [c] is not a valid command.  *)
 val do' : Command.command -> state -> state 
+
+(* Update all the tiles by one move *)
+val update: state -> unit
 
