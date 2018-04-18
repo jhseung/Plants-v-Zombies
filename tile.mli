76a5555c73f4f6701d6418a4f07a854b012911c7
Jaclyn Huang <mh2279@cornell.edu>
2018-04-17 23:05:38 -0400

    Update tile.mli

--- tile.mli
+++ tile.mli
@@ -1,5 +1,6 @@
 type tile = {x: int; y: int; size: int; mutable zombies: zombie list; 
-mutable plant: plant option; mutable left: tile; mutable right: tile}
+mutable plant: plant option; left: tile; right: tile; 
+mutable projectiles: projectile list}
 
 type object = |Zombie of zombie |Projectile of projectile
 
@@ -8,3 +9,5 @@ val init_tile: int -> int -> int -> tile
 (* Checks if an object is on a tile *)
 val on_tile: tile -> object -> bool
 
+(* Update all the mutable types on the tile by one move. *)
+val update: tile -> unit
