e42995197a19962ec311fac39767b543d7fe84a2
Jaclyn Huang <mh2279@cornell.edu>
2018-04-19 13:57:05 -0400

    Update and rename animated.mli to object.mli

--- model/object.mli
+++ model/object.mli
@@ -2,13 +2,20 @@ type info = {speed: int; hp: int}
 
 type mummy = |Normal of info |Conehead of info |Buckethead of info 
 
-type shooter = |Peashooter of info
+type shooter = |Peashooter of info |Cactus of info
 
 type projectile = {shooter: shooter; mutable tile: tile; mutable step: int}
 
 type zombie = {mummy: mummy; mutable pos: tile; mutable hp: int; mutable step: int}
 
 type animated = |Projectile of projectile | Zombie of zombie
+
+type plant = {plant: shooter; mutable tile: tile; mutable HP: int}
+
+type object = |Animated of animated |Static of plant
+
+(* plant a shooter on tile *)
+val: plant: shooter -> tile -> plant
 
 (* Moves the animated from the current tile to the next tile *)
 val next_tile: animated -> unit
