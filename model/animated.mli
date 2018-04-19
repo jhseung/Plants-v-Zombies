b1a8953b93e9058f71ded135fb024c6eea4943d9
Jaclyn Huang <mh2279@cornell.edu>
2018-04-19 13:55:01 -0400

    Update animated.mli

--- model/animated.mli
+++ model/animated.mli
@@ -2,17 +2,17 @@ type info = {speed: int; hp: int}
 
 type mummy = |Normal of info |Conehead of info |Buckethead of info 
 
-type bean = |Peas of info
+type shooter = |Peashooter of info
 
-type projectile = {bean: bean; mutable tile: tile; mutable step: int}
+type projectile = {shooter: shooter; mutable tile: tile; mutable step: int}
 
 type zombie = {mummy: mummy; mutable pos: tile; mutable hp: int; mutable step: int}
 
 type animated = |Projectile of projectile | Zombie of zombie
 
-(* Moves the zombie from the current tile to the next tile *)
+(* Moves the animated from the current tile to the next tile *)
 val next_tile: animated -> unit
 
-(* Update the step count of the zombie by one move (=speed)
-If step > tile.size, moves the zombie to the next tile. *)
+(* Update the step count of animated by one move (=speed)
+If step > tile.size, moves animated to the next tile. *)
 val move: animated -> unit
