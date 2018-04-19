130592038477048ca3b38e78a23efa872f3398e2
Jaclyn Huang <mh2279@cornell.edu>
2018-04-19 13:52:43 -0400

    Update and rename zombie.mli to animated.mli

--- model/animated.mli
+++ model/animated.mli
@@ -2,11 +2,17 @@ type info = {speed: int; hp: int}
 
 type mummy = |Normal of info |Conehead of info |Buckethead of info 
 
+type bean = |Peas of info
+
+type projectile = {bean: bean; mutable tile: tile; mutable step: int}
+
 type zombie = {mummy: mummy; mutable pos: tile; mutable hp: int; mutable step: int}
 
+type animated = |Projectile of projectile | Zombie of zombie
+
 (* Moves the zombie from the current tile to the next tile *)
-val next_tile: zombie -> unit
+val next_tile: animated -> unit
 
 (* Update the step count of the zombie by one move (=speed)
 If step > tile.size, moves the zombie to the next tile. *)
-val move: zombie -> unit
+val move: animated -> unit
