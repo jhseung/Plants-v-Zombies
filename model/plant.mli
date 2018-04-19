f6e746a8e32cd7b1e0773822182bbe8fae0c1720
Jaclyn Huang <mh2279@cornell.edu>
2018-04-19 13:55:37 -0400

    Update plant.mli

--- model/plant.mli
+++ model/plant.mli
@@ -1,7 +1,1 @@
-(*type shooter associates the name of the shooter with damage per shot *)
-type shooter = |Peashooter of int |Cactus of int
 
-type plant = {plant: shooter; mutable tile: tile; mutable HP: int}
-
-(* plant a shooter on tile *)
-val: plant: shooter -> tile -> plant
