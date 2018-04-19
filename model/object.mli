e2f83bd6bc03625c60b5d4f4c699e75bba2d65d3
Jaclyn Huang <mh2279@cornell.edu>
2018-04-19 13:58:48 -0400

    Update object.mli

--- model/object.mli
+++ model/object.mli
@@ -8,18 +8,18 @@ type projectile = {shooter: shooter; mutable tile: tile; mutable step: int}
 
 type zombie = {mummy: mummy; mutable pos: tile; mutable hp: int; mutable step: int}
 
-type animated = |Projectile of projectile | Zombie of zombie
+type moving = |Projectile of projectile | Zombie of zombie
 
 type plant = {plant: shooter; mutable tile: tile; mutable HP: int}
 
-type object = |Animated of animated |Static of plant
+type object = |Moving of moving |Static of plant
 
 (* plant a shooter on tile *)
 val: plant: shooter -> tile -> plant
 
-(* Moves the animated from the current tile to the next tile *)
+(* Moves the moving object from the current tile to the next tile *)
 val next_tile: animated -> unit
 
-(* Update the step count of animated by one move (=speed)
-If step > tile.size, moves animated to the next tile. *)
+(* Update the step count of the moving object by one move (=speed)
+If step >= tile.size, moves moving object to the next tile. *)
 val move: animated -> unit
