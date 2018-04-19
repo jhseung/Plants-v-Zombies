ee356cb00cdc959bfeade119995c0eaddbb1a2cf
Jaclyn Huang <mh2279@cornell.edu>
2018-04-19 13:57:55 -0400

    Update tile.mli

--- model/tile.mli
+++ model/tile.mli
@@ -2,10 +2,6 @@ type tile = {x: int; y: int; size: int; mutable zombies: zombie list;
 mutable plant: plant option; left: tile; right: tile; 
 mutable projectiles: projectile list}
 
-type animated =  |Zombie of zombie |Projectile of projectile
-
-type moving = {id: animated; mutable step: int}
-
 val init_tile: int -> int -> int -> tile
 
 (* Checks if an object is on a tile *)
