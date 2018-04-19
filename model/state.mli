dce1d8c789d307d582d28eab52816e83b5ab28ac
Jaclyn Huang <mh2279@cornell.edu>
2018-04-19 14:39:29 -0400

    Update state.mli

--- model/state.mli
+++ model/state.mli
@@ -3,10 +3,13 @@ type state = {tiles: (Tile.tile array) array; sunlight: int}
 type objects = {zombies: Object.zombie list; plants: Object.plant list; projectiles: Object.projectile list}
 
 (* [init_state col row] is the initial state with col columns and row rows. *)
-val init_state : int -> int -> state
+val init_state: int -> int -> state
 
 (* Returns the number of sunlight. *)
-val get_sunlight : state -> int
+val get_sunlight: state -> int
+
+(* Returns the number of sunflowers. *)
+val get_sunflowers: state -> int
 
 (* Update all the tiles by one move *)
 val update: state -> unit
@@ -23,5 +26,5 @@ val make_plant: Object.shooter -> int -> int -> unit
 (* Updates sthe state with a new zombie at coordinates (x, y) *)
 val make_zombie: Object.mummy -> int -> int -> unit
 
-(* Updates the state with one more sunlight *)
+(* Updates the state with get_sunflowers state + 1 sunlight *)
 val increase_sunlight: state -> unit
