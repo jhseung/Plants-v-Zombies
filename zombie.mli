9e07ae0943936be69a337561dc84143d08105a67
Jaclyn Huang <mh2279@cornell.edu>
2018-04-19 13:29:23 -0400

    Update zombie.mli

--- zombie.mli
+++ zombie.mli
@@ -1,4 +1,12 @@
-type zombie = {mutable pos: tile; mutable hp: int; mutable step: int}
+type info = {speed: int; hp: int}
+
+type mummy = |Normal of info |Conehead of info |Buckethead of info 
+
+type zombie = {mummy: mummy; mutable pos: tile; mutable hp: int; mutable step: int}
 
 (* Moves the zombie from the current tile to the next tile *)
-val move_zombie: zombie -> unit
+val next_tile: zombie -> unit
+
+(* Update the step count of the zombie by one move (=speed)
+If step > tile.size, moves the zombie to the next tile. *)
+val move: zombie -> unit
