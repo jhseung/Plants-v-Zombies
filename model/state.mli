345cded21d421dde6d9d89940d26f3a050792f41
Jaclyn Huang <mh2279@cornell.edu>
2018-04-20 07:24:07 -0400

    Update state.mli

--- model/state.mli
+++ model/state.mli
@@ -1,7 +1,11 @@
+(* total = total number of zombies to be released *)
 type state = {top_left: int*int; tiles: (Tile.tile array) array; 
-mutable sunlight: int}
+mutable sunlight: int; mutable total: int}
 
 type object = |Zombie of zombie |Plant of plant |Projectile of projectile
+
+type objects = {zombies: Object.zombie list; plants: Object.plant list
+projectiles: Object.projectile list *)
 
 type character = |Z of zombie |P of plant
 
@@ -16,7 +20,7 @@ val get_sunlight: state -> int
 val update: state -> unit
 
 (* Returns the objects in the state *)
-val get_objects: state -> object list
+val get_objects: state -> objects
 
 (* Returns the coordinates of the object *)
 val get_coordinates: object -> int*int
@@ -28,7 +32,7 @@ val get_type: object -> string
 (* Updates the state with a new plant at coordinates (x, y) *)
 val make_plant: Object.shooter -> int -> int -> unit
 
-(* Updates sthe state with a new zombie at coordinates (x, y) *)
+(* Updates sthe state with a new zombie at coordinates (x, y), state.total -= 1 *)
 val make_zombie: Object.mummy -> int -> int -> unit
 
 (* Updates the state with a projectile originating from the plant *)
@@ -41,20 +45,23 @@ val sunflower_sunlight: state -> unit
 (* Updates the state with 1 additional sunlight *)
 val increase_sunlight: state -> unit
 
-(* Returns true if all the zombies are dead *)
+(* Returns true if total = 0 and List.length objects.zombies = 0*)
 val has_won: state -> bool 
 
 (* Returns true if a zombie is at the leftmost tile and zombie.step >= tile.size *)
 val has_lost: state -> bool
 
 (* Returns true if the plant is being attacked. *)
-val plant_attacked: plant -> bool
+val plant_attacked: Object.plant -> bool
 
 (* Returns true if the zombie is being attacked. *)
-val zombie_attacked: zombie -> bool
+val zombie_attacked: Object.zombie -> bool
 
 (* Returns true if the zombie is attacking. *)
-val zombie_attacking: zombie -> bool
+val zombie_attacking: Object.zombie -> bool
+
+(* Returns true if the projectile is attacking *)
+val projectile_attacking: projectile -> bool
 
 (* Returns the hp of the character. *)
 val get_hp: character -> int
